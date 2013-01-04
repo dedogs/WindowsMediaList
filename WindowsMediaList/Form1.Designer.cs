03d6509b3a36d7338d074f9bd170b05e707002b5
deDogs <tea@dedogs.com>
2013-01-03 21:18:16 -0800

    Init Files

---
+++ WindowsMediaList/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace WindowsMediaList
+{
+    partial class WMLCreate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WMLCreate));
+            this.FilePath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.create = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.DirTreeView = new System.Windows.Forms.TreeView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // FilePath
+            // 
+            this.FilePath.Location = new System.Drawing.Point(12, 30);
+            this.FilePath.Name = "FilePath";
+            this.FilePath.Size = new System.Drawing.Size(363, 20);
+            this.FilePath.TabIndex = 1;
+            this.FilePath.WordWrap = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Path";
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panel1.Controls.Add(this.create);
+            this.panel1.Location = new System.Drawing.Point(348, 76);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(12);
+            this.panel1.Size = new System.Drawing.Size(132, 295);
+            this.panel1.TabIndex = 5;
+            // 
+            // create
+            // 
+            this.create.Location = new System.Drawing.Point(33, 15);
+            this.create.Name = "create";
+            this.create.Size = new System.Drawing.Size(75, 23);
+            this.create.TabIndex = 5;
+            this.create.Text = "Create";
+            this.create.UseVisualStyleBackColor = true;
+            this.create.Click += new System.EventHandler(this.create_Click_1);
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.DirTreeView);
+            this.panel2.Location = new System.Drawing.Point(12, 76);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(12);
+            this.panel2.Size = new System.Drawing.Size(330, 295);
+            this.panel2.TabIndex = 6;
+            // 
+            // DirTreeView
+            // 
+            this.DirTreeView.Location = new System.Drawing.Point(0, 0);
+            this.DirTreeView.Name = "DirTreeView";
+            this.DirTreeView.Size = new System.Drawing.Size(330, 295);
+            this.DirTreeView.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Results";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(381, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // WMLCreate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 402);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FilePath);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "WMLCreate";
+            this.Text = "Create Windows Media List";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox FilePath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button create;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TreeView DirTreeView;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsMediaList/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml.Linq;
+using System.Text.RegularExpressions;
+using System.Collections;
+
+namespace WindowsMediaList
+{
+    public partial class WMLCreate : Form
+    {
+        public WMLCreate()
+        {
+            InitializeComponent();
+        }
+
+        private void ListDirectory(TreeView treeView, string path)
+        {
+            treeView.Nodes.Clear();
+            var rootDirectoryInfo = new DirectoryInfo(path);
+            treeView.Nodes.Add(CreateDirectoryNode(rootDirectoryInfo));
+        }
+
+        private static TreeNode CreateDirectoryNode(DirectoryInfo directoryInfo)
+        {
+            var directoryNode = new TreeNode(directoryInfo.Name);
+            XDocument xDoc = new XDocument();
+
+            foreach (var directory in directoryInfo.GetDirectories())
+            {
+                directoryNode.Nodes.Add(CreateDirectoryNode(directory));
+            }
+
+            List<XElement> seq = new List<XElement>();
+            bool isFiles = false;
+
+            Regex SpecialChars = new Regex(@"([^a-zA-Z0-9. ])+");
+            Regex isVideoFile = new Regex(@"^[\w\-. ]+(\.mp4|\.wmv|\.mov|\.avi|\.mp3)$");
+            string newFileName;
+
+            foreach (var file in (directoryInfo.GetFiles()).Select(f => f.Name))
+            {
+                if (isVideoFile.IsMatch(file))
+                {
+                    isFiles = true;
+
+                    newFileName = file;
+                    if (SpecialChars.IsMatch(file))
+                    {
+                        newFileName = SpecialChars.Replace(file, "_");
+                        File.Move(directoryInfo.FullName + @"\" + file, directoryInfo.FullName + @"\" + newFileName);
+                    }
+                    seq.Add(new XElement("media", new XAttribute("src", newFileName)));
+
+                    directoryNode.Nodes.Add(new TreeNode(file));
+                }
+            }
+
+            if (isFiles)
+            {
+                xDoc.Add(new XElement("smil", new XElement("head", new XElement("title", directoryInfo.Name)), new XElement("body", new XElement("seq", seq))));
+                xDoc.Save(directoryInfo.FullName + @"\index.wpl");
+            }
+            return directoryNode;
+        }
+
+        private void create_Click_1(object sender, EventArgs e)
+        {
+            ListDirectory(DirTreeView, FilePath.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            FilePath.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+ 
+    }
+}
